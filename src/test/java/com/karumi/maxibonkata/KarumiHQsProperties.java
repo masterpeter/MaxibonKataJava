5b87e53551c98625d26f11f316c1f9f9960a5527
masterpeter <angelo.mastropietro@immobiliare.it>
2017-03-25 12:41:14 +0100

    Edit KarumiHQsProperties.java
    * Add setUp

--- src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -4,11 +4,13 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Angelo Mastropietro on 25/03/17.
@@ -16,17 +18,24 @@ import static junit.framework.TestCase.assertTrue;
 @RunWith(JUnitQuickcheck.class)
 public class KarumiHQsProperties {
 
+    private KarumiHQs karumiHQs;
+
+    @Before
+    public void setUp() throws Exception {
+        karumiHQs = new KarumiHQs(mock(Chat.class));
+    }
+
     @Property
     public void whenMaxibonsTakenSingleDevThanTwoMaxLeft(@From(DevelopersGenerator.class) Developer developer) {
-        KarumiHQs karumiHQs = new KarumiHQs();
         karumiHQs.openFridge(developer);
+
         assertTrue(karumiHQs.getMaxibonsLeft() >= 2);
     }
 
     @Property
     public void whenMaxibonsTakenMultiDevsThanTwoMaxLeft(List<@From(DevelopersGenerator.class) Developer> developers) {
-        KarumiHQs karumiHQs = new KarumiHQs();
         karumiHQs.openFridge(developers);
+
         assertTrue(karumiHQs.getMaxibonsLeft() >= 2);
     }
 }
