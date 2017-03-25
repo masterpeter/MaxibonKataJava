ccdb859e4b605b00f6bd4fa89898cda47c0ea350
masterpeter <angelo.mastropietro@immobiliare.it>
2017-03-25 12:00:47 +0100

    Add first Property test with DeveloperProperties.java

---
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -0,0 +1,28 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by Angelo Mastropietro on 25/03/17.
+ */
+@RunWith(JUnitQuickcheck.class)
+public class DeveloperProperties {
+
+    private static final String DEVELOPER_NAME = "Angelo";
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeveloperProperties.class);
+
+    @Property
+    public void theNumberOfMaxibonsShouldBeGreaterThanZero(int numberOfMaxibons) {
+        LOGGER.debug(String.valueOf(numberOfMaxibons));
+        Developer developer = new Developer(DEVELOPER_NAME, numberOfMaxibons);
+        LOGGER.debug(developer.toString());
+        assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+    }
+}
