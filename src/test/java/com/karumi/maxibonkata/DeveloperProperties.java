3b00bedfc380722cc59eea93072182f079589da2
masterpeter <angelo.mastropietro@immobiliare.it>
2017-03-25 12:11:21 +0100

    Edit DeveloperProperties.java
    * Add unit test for Karumies

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -3,10 +3,12 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -24,5 +26,14 @@ public class DeveloperProperties {
         Developer developer = new Developer(DEVELOPER_NAME, numberOfMaxibons);
         LOGGER.debug(developer.toString());
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+    }
+
+    @Test
+    public void theNumberOfMaxibonsIsCorrectlyAssignedToDevelopers() throws Exception {
+        assertEquals(3, Karumies.PEDRO.getNumberOfMaxibonsToGrab());
+        assertEquals(1, Karumies.ALBERTO.getNumberOfMaxibonsToGrab());
+        assertEquals(0, Karumies.DAVIDE.getNumberOfMaxibonsToGrab());
+        assertEquals(2, Karumies.SERGIO.getNumberOfMaxibonsToGrab());
+        assertEquals(1, Karumies.JORGE.getNumberOfMaxibonsToGrab());
     }
 }
